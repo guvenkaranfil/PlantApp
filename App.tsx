b95db3a736cd8fca34be0cbd51183affd623e8f3
guvenkaranfil <guvenkaranfil235@gmail.com>
2024-09-27 21:35:06 +0300

    feat: initialize react-navigation library for navigation with empty get started screen

--- App.tsx
+++ App.tsx
@@ -1,15 +1,21 @@
-import {View, Text} from 'react-native';
 import React from 'react';
 
 import {Provider} from 'react-redux';
-import {store} from './src/store';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
+import {store} from './src/store';
+import GetStarted from './src/screens/getStarted';
+
+const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <Provider store={store}>
-      <View>
-        <Text>PlantApp</Text>
-      </View>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{headerShown: false}}>
+          <Stack.Screen name="getStarted" component={GetStarted} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </Provider>
   );
 }
---
+++ src/screens/getStarted.tsx
@@ -0,0 +1,18 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+export default function GetStarted() {
+  return (
+    <View style={styles.center}>
+      <Text>Getting Started</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  center: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
