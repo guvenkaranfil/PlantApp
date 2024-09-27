70100d58c26cd9632e3bf56d6568a0d51feadb3a
guvenkaranfil <guvenkaranfil235@gmail.com>
2024-09-28 02:56:59 +0300

    test: remove unncessary wrapper

--- __tests__/screens/getStarted.test.tsx
+++ __tests__/screens/getStarted.test.tsx
@@ -2,12 +2,9 @@ import React from 'react';
 import {render} from '@testing-library/react-native';
 
 import GetStarted from '../../src/screens/getStarted';
-import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 describe('Get Started Screen', () => {
   it('should renders without any warning or error', () => {
-    render(<GetStarted />, {
-      wrapper: children => <SafeAreaProvider>{children}</SafeAreaProvider>,
-    });
+    render(<GetStarted />, {});
   });
 });
