5140df1f5c3496760c5e9e5f3b18c2854dc465a4
guvenkaranfil <guvenkaranfil235@gmail.com>
2024-09-28 04:15:18 +0300

    feat: onboarding design implementation with static data from design

---
+++ src/screens/onboarding/Dots.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+
+import Colors from '../../utils/Colors';
+
+interface IDots {
+  numberOfDots: number;
+  activeIndex: number;
+}
+
+const Dots = ({numberOfDots, activeIndex}: IDots) => {
+  const activeDotSize = 10;
+  const inactiveDotSize = 6;
+  return (
+    <View style={styles.dots}>
+      {Array.from({length: numberOfDots}).map((_, index: number) => {
+        const isActive = activeIndex === index;
+        return (
+          <View
+            testID="dot"
+            key={index}
+            style={{
+              width: isActive ? activeDotSize : inactiveDotSize,
+              height: isActive ? activeDotSize : inactiveDotSize,
+              borderRadius: isActive ? activeDotSize / 2 : inactiveDotSize / 2,
+              backgroundColor: isActive ? Colors.text : Colors.darkIndigo,
+            }}
+          />
+        );
+      })}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  dots: {
+    flexDirection: 'row',
+    gap: 8,
+    alignItems: 'center',
+  },
+});
+
+export default Dots;
---
+++ src/screens/onboarding/datas.ts
@@ -0,0 +1,32 @@
+import {ImageResources} from '../../assets/Generated/ImageResources.g';
+
+export interface IOnboardingData {
+  index: number;
+  titleLeftPrimary: string;
+  titleLeftSecondary: string;
+  titleRight: string;
+  brushSize: number;
+  contentSource: ImageResources;
+  brushRightOffset: number;
+}
+
+export const onboardingDatas: Array<IOnboardingData> = [
+  {
+    index: 0,
+    titleLeftPrimary: 'Take a photo to ',
+    titleLeftSecondary: 'the plant!',
+    titleRight: 'identify',
+    brushSize: 136,
+    contentSource: ImageResources.onboardingfirstbackground,
+    brushRightOffset: -6,
+  },
+  {
+    index: 1,
+    titleLeftPrimary: 'Get plant ',
+    titleLeftSecondary: '',
+    titleRight: 'care guides',
+    brushSize: 152,
+    contentSource: ImageResources.onboardingsecondcontent,
+    brushRightOffset: 6,
+  },
+];
