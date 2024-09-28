b573296159defdbf5719628d9a1c98d94f9e5499
guvenkaranfil <guvenkaranfil235@gmail.com>
2024-09-28 17:36:10 +0300

    refactor: extract static datas into a helper file

--- src/screens/paywall/index.tsx
+++ src/screens/paywall/index.tsx
@@ -14,53 +14,11 @@ import {ImageResources} from '../../assets/Generated/ImageResources.g';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import Sizes from '../../utils/Sizes';
 import Colors from '../../utils/Colors';
-import FeatureCard, {IFeatureCard} from './featureCard';
+import FeatureCard from './featureCard';
 
-import {Scanner, Indicator, Leaf} from '../../assets/icons';
-import OfferOption, {IOfferOption} from './offerOption';
+import OfferOption from './offerOption';
 
-const featuresData: Array<IFeatureCard> = [
-  {
-    icon: Scanner,
-    promotion: {
-      primary: 'Unlimeted',
-      secondary: 'Plant Identify',
-    },
-  },
-  {
-    icon: Indicator,
-    promotion: {
-      primary: 'Faster',
-      secondary: 'Process',
-    },
-  },
-  {
-    icon: Leaf,
-    promotion: {
-      primary: 'Detailed',
-      secondary: 'Plant care',
-    },
-  },
-];
-
-const offerOptions: Array<IOfferOption> = [
-  {
-    id: 1,
-    primaryLabel: '1 Month',
-    secondaryLabel: '$2.99/month, auto renewable',
-    promotion: undefined,
-    period: 'month',
-    price: '$2.99',
-  },
-  {
-    id: 2,
-    primaryLabel: '1 Year',
-    secondaryLabel: 'First 3 days free, then $529,99/year',
-    promotion: 'Save 50%',
-    period: 'year',
-    price: '$529.99',
-  },
-];
+import {featuresData, offerOptions} from './staticDatas';
 
 export default function Paywall() {
   const [selectedOfferID, setSelectedOfferID] = useState(2);
---
+++ src/screens/paywall/staticDatas.ts
@@ -0,0 +1,46 @@
+import {Scanner, Indicator, Leaf} from '../../assets/icons';
+import {IFeatureCard} from './featureCard';
+import {IOfferOption} from './offerOption';
+
+export const featuresData: Array<IFeatureCard> = [
+  {
+    icon: Scanner,
+    promotion: {
+      primary: 'Unlimeted',
+      secondary: 'Plant Identify',
+    },
+  },
+  {
+    icon: Indicator,
+    promotion: {
+      primary: 'Faster',
+      secondary: 'Process',
+    },
+  },
+  {
+    icon: Leaf,
+    promotion: {
+      primary: 'Detailed',
+      secondary: 'Plant care',
+    },
+  },
+];
+
+export const offerOptions: Array<IOfferOption> = [
+  {
+    id: 1,
+    primaryLabel: '1 Month',
+    secondaryLabel: '$2.99/month, auto renewable',
+    promotion: undefined,
+    period: 'month',
+    price: '$2.99',
+  },
+  {
+    id: 2,
+    primaryLabel: '1 Year',
+    secondaryLabel: 'First 3 days free, then $529,99/year',
+    promotion: 'Save 50%',
+    period: 'year',
+    price: '$529.99',
+  },
+];
