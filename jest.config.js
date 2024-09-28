3905af5d736f68e86dce84cbceda21db8237feb5
guvenkaranfil <guvenkaranfil235@gmail.com>
2024-09-28 12:12:28 +0300

    test: configure jest to exclude node_modules

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,11 @@
 module.exports = {
   preset: 'react-native',
   setupFilesAfterEnv: ['./jest-setup.js'],
+  moduleFileExtensions: ['js', 'jsx', 'json', 'ts', 'tsx', 'node'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?@?react-native|@react-native-community|@react-navigation|react-redux)',
+
+    // For pnpm you need to use inlcude `(?!(?:.pnpm/)?` part like this:
+    // 'node_modules/(?!(?:.pnpm/)?((jest-)?@?react-native|@react-native-community|@react-navigation))',
+  ],
 };
