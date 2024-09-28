ddaeb763c81185487bb10665942f697c6e42fd6c
guvenkaranfil <guvenkaranfil235@gmail.com>
2024-09-28 12:16:16 +0300

    feat: Get started screen, should call navigation.replace to navigate to the onboarding screen when the 'Get Started' button is pressed

--- __tests__/screens/getStarted.test.tsx
+++ __tests__/screens/getStarted.test.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
-import {render} from '@testing-library/react-native';
+import {fireEvent, render, screen} from '@testing-library/react-native';
 
 import GetStarted from '../../src/screens/getStarted';
 
 describe('Get Started Screen', () => {
   it('should renders without any warning or error', () => {
-    render(<GetStarted />, {});
+    const actualNav = jest.requireActual('@react-navigation/native');
+    const mockNavigation = {
+      ...actualNav,
+      navigate: jest.fn(),
+      replace: jest.fn(),
+    };
+
+    render(<GetStarted navigation={mockNavigation} />, {});
+  });
+
+  it('should call navigation.replace to navigate to the onboarding screen when the "Get Started" button is pressed', () => {
+    const actualNav = jest.requireActual('@react-navigation/native');
+    const mockNavigation = {
+      ...actualNav,
+      navigate: jest.fn(),
+      replace: jest.fn(),
+    };
+
+    render(<GetStarted navigation={mockNavigation} />, {});
+    const continueButton = screen.getByText(/get started/i);
+
+    fireEvent.press(continueButton);
+
+    expect(mockNavigation.navigate).toHaveBeenCalledTimes(0);
+    expect(mockNavigation.replace).toHaveBeenCalledTimes(1);
+    expect(mockNavigation.replace).toHaveBeenCalledWith('onboarding');
   });
 });
