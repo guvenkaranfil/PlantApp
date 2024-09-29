4524f61addfa7164af2043e8706e96d6cfe56683
guvenkaranfil <guvenkaranfil235@gmail.com>
2024-09-29 17:40:53 +0300

    refactor: install and configure 'babel-plugin-module-resolver' for modular imports

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,22 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        alias: {
+          '@src': './src',
+          '@api': './src/api',
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@navigation': './src/navigation',
+          '@screens': './src/screens',
+          '@store': './src/store',
+          '@utils': './src/utils',
+        },
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    ],
+  ],
 };
