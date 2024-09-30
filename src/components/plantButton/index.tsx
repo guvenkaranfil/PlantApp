475ebe4852b9bf6c56095057f1cb91e26400af7c
guvenkaranfil <guvenkaranfil235@gmail.com>
2024-09-30 14:17:20 +0300

    refactor: extend 'PlantButton' accept children with label and document props

--- src/components/plantButton/index.tsx
+++ src/components/plantButton/index.tsx
@@ -12,28 +12,63 @@ import {
 import colors from '@src/theme/colors';
 import fontSizes from '@src/theme/fontSizes';
 
-interface IPlantButton {
+interface IPlantButtonBase {
   testID?: string;
   hitSlop?: PressableProps['hitSlop'];
   style?: StyleProp<ViewStyle>;
   labelStyle?: StyleProp<TextStyle>;
-  label: string;
+  children?: React.ReactNode;
+  shouldDefaultStyle?: boolean;
   onPress?: () => void;
 }
 
+interface IPlantButtonWithLabel extends IPlantButtonBase {
+  label: string;
+  labelStyle?: StyleProp<TextStyle>;
+  children?: never;
+}
+
+interface IPlantButtonWithChildren extends IPlantButtonBase {
+  label?: never;
+  children: React.ReactNode;
+}
+
+type IPlantButton = IPlantButtonWithLabel | IPlantButtonWithChildren;
+
+/**
+ * A customizable button component uses 'Pressable' that can render either a label or children.
+ *
+ * @param {Object} props - The props for the PlantButton component.
+ * @param {string} [props.testID] - Optional test ID for testing purposes.
+ * @param {PressableProps['hitSlop']} [props.hitSlop] - Hit slop for the Pressable component.
+ * @param {StyleProp<ViewStyle>} [props.style] - Custom style for the button.
+ * @param {StyleProp<TextStyle>} [props.labelStyle] - Custom style for the label text.
+ * @param {React.ReactNode} [props.children] - Optional children to be rendered inside the button.
+ * @param {boolean} [props.shouldDefaultStyle=true] - If true, applies default styling to the button.
+ * @param {() => void} [props.onPress] - Called when a single tap gesture is detected.
+ * @param {string} [props.label] - Required label text to display when children are not provided.
+ * @example
+ * ```tsx
+ * <PlantButton label="Get Started" onPress={startOnBoarding} />
+ * ```
+ * @returns {JSX.Element} The rendered button component.
+ *
+ */
 export default function PlantButton({
   testID,
   style,
   labelStyle,
   label,
+  children,
+  shouldDefaultStyle = true,
   onPress,
 }: IPlantButton) {
   return (
     <Pressable
       testID={testID}
-      style={[styles.startButton, style]}
+      style={[shouldDefaultStyle ? styles.startButton : null, style]}
       onPress={onPress}>
-      <Text style={[styles.startLabel, labelStyle]}>{label}</Text>
+      {children ?? <Text style={[styles.startLabel, labelStyle]}>{label}</Text>}
     </Pressable>
   );
 }
