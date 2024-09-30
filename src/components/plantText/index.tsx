1b32904b955b2bd218aab459e8321625e0f05c2f
guvenkaranfil <guvenkaranfil235@gmail.com>
2024-09-30 18:56:01 +0300

    doc: document 'PlantText' component usage

--- src/components/plantText/index.tsx
+++ src/components/plantText/index.tsx
@@ -31,6 +31,72 @@ interface IPlantTextWithChildren extends IPlantTextBase {
 
 type IPlantText = IPlantTextWithLabel | IPlantTextWithChildren;
 
+/**
+ * Base interface for the PlantText component, defining common text styling properties.
+ *
+ * @interface IPlantTextBase
+ * @property {IColors} [colorName] - Defines the color of the text. The value must be a key from the `colors` object, formatted as 'color.category' (e.g., 'green.main').
+ * @property {IFonts} [fontFamily] - Defines the font family of the text. This corresponds to available font names in the `fonts` object.
+ * @property {IFontSizes} [fontSize] - Specifies the font size of the text. The value comes from the `fontSizes` object.
+ * @property {number} [top] - Optional margin at the top of the text.
+ * @property {number} [marginHorizontal] - Optional horizontal margin for the text.
+ * @property {number} [paddingTop] - Optional padding at the top of the text.
+ * @property {number} [paddingBottom] - Optional padding at the bottom of the text.
+ * @property {number} [letterSpacing] - Optional letter spacing for the text.
+ * @property {number} [lineHeight] - Optional line height for the text.
+ * @property {TextStyle['textAlign']} [textAlign] - Specifies the alignment of the text (e.g., 'left', 'center', 'right').
+ * @property {React.ReactNode} [children] - Optional children elements that can be rendered inside the text.
+ */
+
+/**
+ * Interface for the PlantText component when using a label as a string.
+ *
+ * @interface IPlantTextWithLabel
+ * @extends IPlantTextBase
+ * @property {string} label - Required label text to display. If this is provided, `children` should not be used.
+ */
+
+/**
+ * Interface for the PlantText component when using children as React elements.
+ *
+ * @interface IPlantTextWithChildren
+ * @extends IPlantTextBase
+ * @property {React.ReactNode} children - Required children to be rendered inside the text. If this is provided, `label` should not be used.
+ */
+
+/**
+ * Type definition for the PlantText component, which can either use a label or children for rendering text.
+ *
+ * @typedef {IPlantTextWithLabel | IPlantTextWithChildren} IPlantText
+ * @example
+ * ```tsx
+ * <PlantText label="Hello World" colorName="green.main" />
+ * <PlantText colorName="black.gray">Hello World</PlantText>
+ * ```
+ */
+
+/**
+ * A customizable Text component uses 'Text', that can render either a label or children.
+ *
+ * @param {Object} props - The props for the PlantText component.
+ * @param {IColors} [colorName] - Defines the color of the text. The value must be a key from the `colors` object, formatted as 'color.category' (e.g., 'green.main').
+ * @param {IFonts} [fontFamily] - Defines the font family of the text. This corresponds to available font names in the `fonts` object.
+ * @param {IFontSizes} [fontSize] - Specifies the font size of the text. The value comes from the `fontSizes` object.
+ * @param {number} [top] - Optional margin at the top of the text.
+ * @param {number} [marginHorizontal] - Optional horizontal margin for the text.
+ * @param {number} [paddingTop] - Optional padding at the top of the text.
+ * @param {number} [paddingBottom] - Optional padding at the bottom of the text.
+ * @param {number} [letterSpacing] - Optional letter spacing for the text.
+ * @param {number} [lineHeight] - Optional line height for the text.
+ * @param {TextStyle['textAlign']} [textAlign] - Specifies the alignment of the text (e.g., 'left', 'center', 'right').
+ * @param {React.ReactNode} [children] - Optional children elements that can be rendered inside the text.
+ * @param {IPlantTextWithLabel | IPlantTextWithChildren} IPlantText
+ * @example
+ * ```tsx
+ * <PlantText label="Hello World" colorName="green.main" />
+ * <PlantText colorName="black.gray">Hello World</PlantText>
+ * ```
+*/
 export default function PlantText({
   label,
   colorName,
